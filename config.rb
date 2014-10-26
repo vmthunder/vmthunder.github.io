7aa178b52705c3a121aefc9dc485c0183ab8b528
zhang john <zhang.john.vmthunder@gmail.com>
2014-10-26 09:13:17 -0400

    add vmthunder.source

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/VMThunder/"
+http_images_path = "/VMThunder/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/VMThunder/fonts"
+css_dir = "public/VMThunder/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
